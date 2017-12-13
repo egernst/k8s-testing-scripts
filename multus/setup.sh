2ed6662fd5e8c461ef539d344982b41dbf4d9fe5
Eric Ernst <eric.ernst@intel.com>
2017-12-13 15:55:19 -0800

    remove old directory/path that we don't use...
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- multus/setup.sh
+++
@@ -1,8 +0,0 @@
-
-master=$(hostname)
-sudo -E kubectl taint nodes "$master" node-role.kubernetes.io/master:NoSchedule-
-
-sudo -E kubectl create -f crdnetwork.yaml
-sudo -E kubectl create -f ngic-networks.yaml 
-sudo -E kubectl create -f ngic-pods.yaml 
-
